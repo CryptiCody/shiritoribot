30e80fe482c40c3d18a7565f437fb394f6111ed6
CrypticCabub <CrypticCabub@gmail.com>
2021-03-27 14:55:10 -0400

    initial commit

---
+++ src/main/java/edu/regent/shiritoribot/ShiritoriGame.java
@@ -0,0 +1,27 @@
+package edu.regent.shiritoribot;
+
+public class ShiritoriGame {
+
+    public void handleUserWord(String wordSubmission) {
+
+        try{
+            submitWord("myWord");
+        } catch (EliminationException e) {
+            eliminatePlayer();
+        }
+
+        if(isLegalWord(wordSubmission)) {
+            nextPlayer();
+        } else {
+            eliminatePlayer();
+        }
+    }
+
+    public boolean submitWord(String word) throws EliminationException {
+        if(!startsWithSameLetterAsPreviousWordEndedWith(word)) return false;
+        if(hasBeenUsedBefore(word)) return false;
+        if(!isInLegalWordDictionary(word)) return false;
+        return true;
+    }
+
+}
